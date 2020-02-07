10d674169896392ce3249f18583d75f59c713b35
Sabinca <kaiser2169@gmail.com>
2020-02-07 11:50:45 +0300

    Финальный

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const routes = require("./routes");
 
 let app = express();
 const port = 80;
+
 //подключаем шаблонизатор
 app.set("view engine","hbs");
+
 
 app.set("view options", {layout:"layout"});
 //подключаем маршруты
 app.use("/", routes);
+app.use(express.static(__dirname +"/css"));
 
 app.listen(port,() =>{
-    console.log("app run on https://localhost");
+    console.log("app run on http://localhost");
 });
